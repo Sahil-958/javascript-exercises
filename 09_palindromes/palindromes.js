30773471942d12209d9d1d65340b824c34e4972b
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-08-17 17:58:36 +0530

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let  pstr=str.toLowerCase().replace(/[^a-z0-9]/g,'');
+return pstr===(pstr.split('').reverse().join(''));
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrObj) {
+return arrObj.reduce(function(oldest,current){
+    if(!current.yearOfDeath) current.yearOfDeath=new Date().getFullYear(); 
+    let currentAge=current.yearOfDeath-current.yearOfBirth;
+    let oldestAge=oldest.yearOfDeath-oldest.yearOfBirth;
+    return oldestAge < currentAge ? current : oldest;
+},arrObj[0]);
 };
 
 // Do not edit below this line
